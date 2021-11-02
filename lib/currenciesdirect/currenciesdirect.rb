98f9be061a2310c2422e453f4447c173295f8925
Jason Crockett <JesusisLord33333@outlook.com>
2021-11-02 00:21:23 +0000

    quotes implemented

--- lib/currenciesdirect/currenciesdirect.rb
+++ lib/currenciesdirect/currenciesdirect.rb
@@ -11,7 +11,7 @@ module Currenciesdirect
 
       def initialize(client_id,client_secret,scope)
         @@token = self.class.post('/auth/oauth/v2/token',{options: {'grant_type' => client_credentials,"client_id" => client_id,"client_secret" => client_secret,"scope" => scope}})
-        @header =  {'Authorization' => "Bearer "+@@token,'Content-Type' => 'application/json'}
+        @headers =  {'Authorization' => "Bearer "+@@token, 'Content-Type' => 'application/json'}
       end
 
       #
@@ -19,9 +19,19 @@ module Currenciesdirect
       # https://github.com/jnunemaker/httparty/blob/8ceb806b09d0e14ee5be00804a4fc2ce69eae079/spec/httparty/hash_conversions_spec.rb
       #
       def rates(pairs:array)
-        self.class.get('/v1/rates',{options:{'pair' => pairs},headers:@header})
+        self.class.get('/v1/rates',{options:{'pair' => pairs},headers:@headers})
       end
 
+
+      # Fixed Side Sell should be true if your selling a set amount of currency
+      def quotes(sell_currency,buy_currency,amount ,customerId,fixed_side_sell = true)
+        if fixed_side_sell
+          fixed_side = 'SELL'
+        else
+          fixed_side = 'BUY'
+        end
+        self.class.post("/v1/customers/#{customerId}/quotes",{body:{'sell_currency' => sell_currency,'buy_currency' => buy_currency,'amount' => amount,'fixed_side' => fixed_side},headers:@headers.merge({'Accept' => 'application/json'})})
+      end
     end
     class Sandbox < Client
       base_url 'https://sandbox.currenciesdirect.com'
